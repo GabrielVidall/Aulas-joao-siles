d43c1fa29816fd3780cc7a54f8c7c30e7c08dad4
GabrielVidall <gabriel_vdl@hotmail.com>
2022-08-08 21:58:32 -0300

    Aula 01 - introdução ao JS

---
+++ PMII/Aula01/teste.js
@@ -0,0 +1,17 @@
+var a = 3 // declarando variavel com var
+let b = 4 // declarando variavel com let (recomendavel por ser mais moderno)
+console.log('conteudo de a: ', a, 'conteudo de b: ', b)
+
+var a = 30 // var pode ser redeclarada duas vezes
+b = 40 // let é declarada uma unica vez
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5 // declaração de constante, usar para coisas que nao tem 
+// c = 50 // não consegue reatribuir valor
+console.log(c)
